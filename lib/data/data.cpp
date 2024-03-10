306dc3ac6032be628629b4c71cbdd1d6e27fd441
Snail Mail <>
2024-03-10 13:27:08 -0400

    data.cpp

---
+++ lib/data/data.cpp
@@ -0,0 +1,104 @@
+#include "data.h"
+
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+void Data::save_data() {
+  SPIFFS.begin();
+  File file = SPIFFS.open("/data.txt", "w");
+  StaticJsonDocument<384> doc;
+
+  doc["devEUI"] = this->devEui;
+  doc["joinEUI"] = this->joinEui;
+  doc["appkey"] = this->appKey;
+  doc["ssid"] = this->ssid;
+  doc["pass"] = this->password;
+  doc["sda"] = this->sda;
+  doc["scl"] = this->scl;
+  doc["mosfet"] = this->mosfet;
+  doc["configured"] = this->configured;
+
+  serializeJson(doc, file);
+  file.close();
+  SPIFFS.end();
+}
+
+void Data::load_data() {
+  SPIFFS.begin();
+  File file = SPIFFS.open("/data.txt", "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  StaticJsonDocument<384> doc;
+
+  DeserializationError error = deserializeJson(doc, file);
+
+  if (error) {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+  this->devEui = str_to_byte_array(doc["devEui"]);
+  this->joinEui = str_to_byte_array(doc["joinEui"]);
+  this->appKey = str_to_byte_array(doc["appKey"]);
+  strcpy(this->ssid, doc["ssid"]);
+  strcpy(this->password, doc["password"]);
+  this->sda = doc["sda"];
+  this->scl = doc["scl"];
+  this->mosfet = doc["mosfet"];
+  this->configured = doc["configured"];
+  file.close();
+  SPIFFS.end();
+}
+
+uint8_t* Data::str_to_byte_array(const char* str) {
+  size_t strLen = strlen(str);
+  size_t numBytes = strLen / 2;
+  uint8_t* byteArray = new uint8_t[numBytes];
+  for (size_t i = 0; i < numBytes; ++i) {
+    sscanf(&str[i * 2], "%2hhx", &byteArray[i]);
+  }
+  return byteArray;
+}
+
+Data::Data() {
+  if (this->devEui == nullptr) this->load_data();
+}
+
+uint8_t* Data::get_devEui() { return this->devEui; }
+
+uint8_t* Data::get_joinEui() { return this->joinEui; }
+
+uint8_t* Data::get_appkey() { return this->appKey; }
+
+char* Data::get_ssid() { return this->ssid; }
+
+char* Data::get_password() { return this->ssid; }
+
+uint8_t Data::get_configured() { return this->configured; }
+
+uint8_t Data::get_sda_pin() { return this->sda; }
+
+uint8_t Data::get_scl_pin() { return this->scl; }
+
+uint8_t Data::get_mosfet_pin() { return this->mosfet; }
+
+void Data::set_devEui(char* devEui) {
+  if (devEui == nullptr) return;
+  if (this->devEui != nullptr) delete[] this->devEui;
+  this->devEui = str_to_byte_array(devEui);
+}
+
+void Data::set_joinEui(char* joinEui) {
+  if (joinEui == nullptr) return;
+  if (this->joinEui != nullptr) delete[] this->joinEui;
+  this->joinEui = str_to_byte_array(joinEui);
+}
+
+void Data::set_appkey(char* appKey) {
+  if (appKey == nullptr) return;
+  if (this->appKey != nullptr) delete[] this->appKey;
+  this->appKey = str_to_byte_array(appKey);
+}
+
+void Data::set_configured(uint8_t configured) {}
---
+++ lib/data/data.h
@@ -0,0 +1,40 @@
+#ifndef __data__
+#define __data__
+#include <Arduino.h>
+class Data {
+ private:
+  static uint8_t* devEui;
+  static uint8_t* joinEui;
+  static uint8_t* appKey;
+  static char ssid[32];
+  static char password[64];
+  static uint8_t sda;
+  static uint8_t scl;
+  static uint8_t mosfet;
+  static uint8_t configured;
+  void load_data();
+  uint8_t* str_to_byte_array(const char* str);
+
+ public:
+  // constructor
+  Data();
+  // save object to file
+  void save_data();
+  // getters and setters
+  uint8_t* get_devEui();
+  uint8_t* get_joinEui();
+  uint8_t* get_appkey();
+  char* get_ssid();
+  char* get_password();
+  uint8_t get_configured();
+  uint8_t get_sda_pin();
+  uint8_t get_scl_pin();
+  uint8_t get_mosfet_pin();
+  uint8_t get_configured();
+  void set_devEui(char* devEui);
+  void set_joinEui(char* joinEui);
+  void set_appkey(char* appKey);
+  void set_configured(uint8_t configured);
+};
+
+#endif
